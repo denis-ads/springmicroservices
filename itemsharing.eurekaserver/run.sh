8f1f73ddf5e00e2424856f16957893367852012d
DENIS APOLINARIO DA SILVA <denis.apolinario@viavarejo.com.br>
2018-01-01 21:40:21 -0200

    remoção eureka para uso no openshift

--- itemsharing.eurekaserver/run.sh
+++
@@ -1,4 +0,0 @@
-echo "*************************************************"
-echo "Staring eureka server"
-echo "*************************************************"
-java -Djava.security.egd=file:/dev./urandom -jar /usr/local/eurekaservice/@project.build.finalName@.jar
--- itemsharing.eurekaserver/src/main/docker/run.sh
+++
@@ -1,4 +0,0 @@
-echo "*************************************************"
-echo "Staring eureka server"
-echo "*************************************************"
-java -Djava.security.egd=file:/dev./urandom -Xmx256m -Dspring.profiles.active=docker -jar /usr/local/eurekaservice/@project.build.finalName@.jar
